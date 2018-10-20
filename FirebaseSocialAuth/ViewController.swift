97e9d2b4b23f5477bfe886b1f9bbdfcbe2bb9036
ikechukwuhenry <ikechukwuhenry@ymail.com>
2018-10-20 19:35:31 +0100

    Initial Commit

---
+++ FirebaseSocialAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSocialAuth
+//
+//  Created by ikechukwu Michael on 20/10/2018.
+//  Copyright © 2018 ikechukwu Michael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
